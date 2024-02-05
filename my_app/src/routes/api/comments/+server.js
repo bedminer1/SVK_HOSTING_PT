e0fc04838f9f76da3c94b52173137418b2be6824
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-05 18:14:05 +0800

    learnt to handle POST requests

--- my_app/src/routes/api/comments/+server.js
+++ my_app/src/routes/api/comments/+server.js
@@ -10,3 +10,14 @@ export function GET() {
     // does the same thing as below
     return json(comments)
 }
+
+export async function POST(requestEvent) {
+    const { request } = requestEvent
+    const { text } = await request.json()
+    const newComment = {
+        id: comments.length + 1,
+        text
+    }
+    comments.push(newComment)
+    return json(newComment, { status: 201 })
+}
