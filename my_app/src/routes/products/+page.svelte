2f80b2719d3f3397b3a7f43a462dd21d2bc522ad
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-05 11:42:12 +0800

    learnt about route matchers, using regex

---
+++ my_app/src/routes/products/+page.svelte
@@ -0,0 +1,13 @@
+
+<div class="flex flex-col justify-center h-full text-2xl">
+    <h1 class="text-3xl">Product List</h1>
+    <div>
+        <p><a href="/products/1">Product 1</a></p>
+    </div>
+    <div>
+        <p><a href="/products/2">Product 2</a></p>
+    </div>
+    <div>
+        <p><a href="/products/3">Product 3</a></p>
+    </div>
+</div>
---
+++ my_app/src/routes/products/[productId=integer]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { page } from '$app/stores'
+    const productId = $page.params.productId
+
+</script>
+
+<div>
+    <h1>Details about product {productId}</h1>
+</div>
