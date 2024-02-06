dd9f4072979fb879813c273cad7c197b41929875
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-06 21:54:10 +0800

    learnt to use form actions to post data without js on browser

--- my_app/src/routes/(auth)/login/+page.svelte
+++ my_app/src/routes/(auth)/login/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Login</h1>
+<script>
+    export let form
+</script>
+
+
+<div class="flex items-center justify-center h-full text-2xl">
+    <form method="post" class="flex flex-col gap-2">
+        <p>{form?.message || ''}</p>
+        <input type="text" name="username" placeholder="Username" class="px-2 text-gray-700">
+        <input type="password" name="password" placeholder="Password" class="px-2 text-gray-700">
+        <button type='submit'>Login</button>
+    </form>
+</div>
